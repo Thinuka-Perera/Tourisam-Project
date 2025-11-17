7c5220e386a584d114bbb66d2d4bbd2a6e9ee5d8
Thinuka-Perera <thinukapereraa@gmail.com>
2025-11-17 20:35:31 +0530

    Move large media files to Git LFS

---
+++ constant/constant.ts
@@ -0,0 +1,32 @@
+export const navLinks = [
+  {
+    id: 1,
+    url: "/",
+    label: "Home",
+  },
+  {
+    id: 2,
+    url: "About",
+    label: "About",
+  },
+  {
+    id: 3,
+    url: "Destination",
+    label: "Destination",
+  },
+  {
+    id: 4,
+    url: "Blog",
+    label: "Blog",
+  },
+  {
+    id: 5,
+    url: "Dashboard",
+    label: "Dashboard",
+  },
+  {
+    id: 6,
+    url: "Contact",
+    label: "Contact",
+  },
+];
