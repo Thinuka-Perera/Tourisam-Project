bfe992686665837a23be16c3ae368d45f701b8e3
Thinuka-Perera <thinukapereraa@gmail.com>
2025-11-18 00:05:16 +0530

    Navbar is complete and Hero Section is design

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,15 +1,39 @@
+"use client";
 import { navLinks } from "@/constant/constant";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { HiBars3BottomRight } from "react-icons/hi2";
 import { TbAirBalloon } from "react-icons/tb";
 
-const Nav = () => {
+
+type Props = { // for places of Props we can use any name
+
+  openNav:() => void
+
+}
+
+
+const Nav = ({openNav}:Props) => {
+
+
+  const  [navBg, setNavBg] = useState(false);
+
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY >= 90) setNavBg(true);
+      if (window.scrollY < 90) setNavBg(false)
+    };
+    window.addEventListener("scroll",handler);
+    return () => window.removeEventListener("scroll",handler);
+  },[])
+
+
   return (
-    <div className="bg-blue-950 transition-all duration-200 h-[12vh] z-[1000]">
+    <div className={` ${navBg ? 'bg-blue-950 shadow-md ' : "fixed"}  transition-all duration-200 h-[12vh] z-[1000] fixed w-full`}>
       <div className="flex items-center h-full justify-between w-[90%] xl:w[80%] mx-auto">
         {/* logo */}
-        <div className="flex items-center space-x-2">
-          <div className="w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center flex-col">
+        <div className="flex items-center space-x-2 ">
+          <div className="w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center flex-col ">
             <TbAirBalloon className="w-6 h-6 text-white" />
           </div>
           <h1 className="text-xl md:text-2xl text-white font-bold">
@@ -22,7 +46,8 @@ const Nav = () => {
           {navLinks.map((link) => {
             return (
               <Link href={link.url} key={link.id}>
-                <p className="relative text-white text-base font-medium w-fit block
+                <p
+                  className="relative text-white text-base font-medium w-fit block
                       after:block after:content-[''] after:absolute after:left-0 after:-bottom-1
                       after:h-[3px] after:w-full after:bg-yellow-300 
                       after:scale-x-0 hover:after:scale-x-100 
@@ -35,14 +60,24 @@ const Nav = () => {
           })}
         </div>
 
-
         {/* Nav Button   */}
         <div className="flex items-center space-x-4">
-            <button></button>  
-        </div>  
+          <button
+            className="
+                md:px-12 md:py-2.5 
+                px-8 py-2 
+                bg-white hover:bg-gray-200 
+                text-black text-base
+                transition-all duration-200 
+                rounded-lg
+              "
+          >
+            Book Now
+          </button>
 
-
-
+          {/* Burger Menu */}
+          <HiBars3BottomRight onClick={openNav} className="w-8 h-8 cursor-pointer  text-white lg:hidden"/>
+        </div>
       </div>
     </div>
   );
