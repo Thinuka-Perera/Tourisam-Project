7715ec5122fdba0127bfe782948f77a258d2e592
Thinuka-Perera <thinukapereraa@gmail.com>
2025-11-17 19:44:47 +0530

    Initialize the project

---
+++ components/Home/Navbar/Nav.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { TbAirBalloon } from 'react-icons/tb'
+
+const Nav = () => {
+  return (
+    <div className='bg-blue-950 transition-all duration-200 h-[12vh] z-[1000]'>
+      <div className='flex items-center h-full justify-between w-[90%] xl:w[80%] mx-auto'> 
+        {/* logo */}
+        <div className='flex items-center space-x-2'>
+            <div className='w-10 h-10 bg-rose-500 rounded-full flex items-center justify-center flex-col'>
+                <TbAirBalloon className='w-6 h-6 text-white'/>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
